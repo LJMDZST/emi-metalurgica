46b2cffded16da2dafe923f2b4a77a517c4371a7
purklestudio <purklestudio@gmail.com>
2024-05-17 00:54:20 -0300

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -30,13 +30,13 @@ export const Navbar = () => {
 
   return (
     <nav className="row align-items-center justify-content-start gx-5 " style={{height:"100%"}}>
-      <div className=" col-md-2 text-center "
+      {/* <div className=" col-md-2 text-center "
         onMouseOver={handleMouseOver}
         onMouseLeave={e => handleMouseLeave(e,2)}
       >
         <Link className='item-menu' to={"about"}  style={{textDecoration:"none"}} onClick={handleInstitucional}> INSTITUCIONAL </Link>
         
-      </div>  
+      </div>  */}
       <div className=" col-md-2  text-center"
                 onMouseOver={handleMouseOver}
                 onMouseLeave={e => handleMouseLeave(e,2)}
